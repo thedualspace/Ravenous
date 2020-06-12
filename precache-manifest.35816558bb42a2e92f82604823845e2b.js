8f67d2267096f60184cc66b5daa495ba9f8e8d7c
thedualspace <64597895+thedualspace@users.noreply.github.com>
2020-06-12 18:04:09 +0100

    Updates

---
+++ precache-manifest.35816558bb42a2e92f82604823845e2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b8c1a89616114d8c0080f9b1a396f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87957a3d94f61c92b793",
+    "url": "/static/css/main.7dbcec8e.chunk.css"
+  },
+  {
+    "revision": "671f533de51a883c15e0",
+    "url": "/static/js/2.80b49be5.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.80b49be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87957a3d94f61c92b793",
+    "url": "/static/js/main.a196ab7a.chunk.js"
+  },
+  {
+    "revision": "f08bef91f12dbf928030",
+    "url": "/static/js/runtime-main.6325cf30.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
